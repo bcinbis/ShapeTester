99cac2e4bc4a1fddcb8511e960d9206c75d4a611
bcinbis <31314514+bcinbis@users.noreply.github.com>
2018-02-13 08:30:59 -0700

    Add files via upload

---
+++ ShapeTester/Box.py
@@ -0,0 +1,19 @@
+class Cube(object):
+    volume = 0
+    SA = 0
+
+    def __init__(self):
+        self.w = 0
+        self.l = 0
+        self.h = 0
+
+    def volume(self):
+        volume = self.w * self.l * self.h
+        print("Your volume is: " + volume)
+
+    def SA(self):
+        SA = 2*self.w*self.l + 2*self.l*self.h + 2*self.w*self.h
+        print("Your surface area is: " + SA)
+    
+    
+          
